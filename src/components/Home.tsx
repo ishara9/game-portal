4810409dd231c80fe2db74aad165573596fe6b10
ishara <ishara9@gmail.com>
2024-05-30 09:30:30 +0530

    grid layout using Chakra ui

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,27 @@
 import GameMenu from "./GameMenu";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 const Home = () => {
   return (
     <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
       <GameMenu />
       <div>Home</div>
       <Button colorScheme="blue">Submit</Button>
