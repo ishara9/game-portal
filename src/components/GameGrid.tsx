31f8e57e9c9b7c047e3da39b756b2c5c8adfb78f
Ishara Abeysekera <ishara.abeysekera@endowus.com>
2024-07-13 10:59:31 +1200

    Generic fetch

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import GameCardContainer from "./GameCardContainer";
 import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeltons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
@@ -24,7 +24,7 @@ const GameGrid = () => {
               <GameSkeltonCard key={skeleton} />
             </GameCardContainer>
           ))}
-        {games?.map((game) => (
+        {data?.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
           </GameCardContainer>
