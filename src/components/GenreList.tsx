2e841dae2ad9bd4d9b82f941318f9bb6c15aa608
Ishara Abeysekera <ishara.abeysekera@endowus.com>
2024-07-13 11:25:03 +1200

    fix alignments with genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,27 @@
+import { VStack, Text, HStack, Image, List, ListItem } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedURL from "./util/ImageURL";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius="8px"
+              src={getCroppedURL(genre.image_background)}
+            />
+            <Text fontSize="lg" key={genre.id}>
+              {genre.name}
+            </Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
