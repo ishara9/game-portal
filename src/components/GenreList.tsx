d3e9327ff678a4f1a1a0870264e8a5882d84995f
Ishara Abeysekera <ishara.abeysekera@endowus.com>
2024-07-13 22:30:32 +1200

    Genre alignments

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -7,6 +7,7 @@ import {
   ListItem,
   Spinner,
   Button,
+  Heading,
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedURL from "./util/ImageURL";
@@ -27,28 +28,36 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   };
 
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius="8px"
-              src={getCroppedURL(genre.image_background)}
-            />
-            <Button
-              onClick={() => handleClick(genre)}
-              variant="link"
-              fontSize="lg"
-              fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
-              key={genre.id}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBottom={2}>
+        Genres
+      </Heading>
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius="8px"
+                src={getCroppedURL(genre.image_background)}
+                objectFit="cover"
+              />
+              <Button
+                onClick={() => handleClick(genre)}
+                variant="link"
+                fontSize="lg"
+                fontWeight={selectedGenre?.id === genre.id ? "bold" : "normal"}
+                key={genre.id}
+                whiteSpace="normal"
+                textAlign="left"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
